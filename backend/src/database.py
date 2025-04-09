da80837a84fa672894541bc4bccd3b386ea125fe
mishkowsky <aleksandrov.m2542@gmail.com>
2025-04-09 04:45:48 +0300

    fix database connection

--- backend/src/database.py
+++ backend/src/database.py
@@ -9,46 +9,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 load_dotenv()
 
-
-@dataclass(frozen=True)
-class DBConfig:
-    DBMS: str
-    DRIVER: str
-    HOSTNAME: str
-    DATABASE: str
-    USERNAME: str
-    PASSWORD: str
-    config_name: str
-    PORT: str | None = None
-
-
-class DBConfigInstance:
-
-    def __init__(self, in_db_config: DBConfig):
-        if in_db_config.PORT is None:
-            self.DB_URI = '{}+{}://{}:{}@{}/{}'.format(
-                in_db_config.DBMS, in_db_config.DRIVER, in_db_config.USERNAME,
-                in_db_config.PASSWORD, in_db_config.HOSTNAME, in_db_config.DATABASE
-            )
-        else:
-            self.DB_URI = '{}+{}://{}:{}@{}:{}/{}'.format(
-                in_db_config.DBMS, in_db_config.DRIVER, in_db_config.USERNAME,
-                in_db_config.PASSWORD, in_db_config.HOSTNAME, in_db_config.PORT, in_db_config.DATABASE
-            )
-
-
-DB_CONFIG = DBConfigInstance(
-    DBConfig(
-        DBMS=os.getenv("DBMS", "postgresql"),
-        DRIVER=os.getenv("DB_DRIVER", "psycopg2"),
-        HOSTNAME=os.getenv("DB_HOSTNAME", "localhost"),
-        DATABASE=os.getenv("DB_DATABASE", "todo-app"),
-        USERNAME=os.getenv("DB_USERNAME", "postgres"),
-        PASSWORD=os.getenv("DB_PASSWORD", "postgres"),
-        config_name='prod_config'
-    ))
-
-engine = create_engine(DB_CONFIG.DB_URI)
+engine = create_engine(os.getenv("DB_URI"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,14 +1,19 @@
+import os
+
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 
-from src.database import Base
-from src.main import get_db, app, engine
+
+@pytest.fixture(scope="module")
+def env_vars():
+    os.environ["DB_URI"] = "sqlite:///./test.db"
 
 
 @pytest.fixture(scope="module")
-def test_db():
+def test_db(env_vars):
+    from src.database import Base
     engine = create_engine("sqlite:///./test.db")
     Base.metadata.create_all(bind=engine)
     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -20,6 +25,7 @@ def test_db():
 
 @pytest.fixture(scope="module")
 def client(test_db):
+    from src.main import get_db, app, engine
     app.dependency_overrides[get_db] = lambda: test_db
     app.dependency_overrides[engine] = create_engine("sqlite:///./test.db")
     yield TestClient(app)
